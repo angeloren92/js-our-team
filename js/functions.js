50badbdf73c08a411420ed82320f20269611b287
angeloren92 <angeloren92@gmail.com>
2026-02-18 14:55:30 +0100

    add rensponsive

--- js/functions.js
+++ js/functions.js
@@ -4,14 +4,14 @@ function getMarkup(arr) {
     for (let i = 0; i < arr.length; i++) {
         const {name, role, email, img} = arr[i];
         markup += `
-        <div class="col" >
-            <div class="card bg-dark rounded-0 border-0 text-white">
+        <div class="col-md-6 col-xl-4 gy-3" >
+            <div class="card bg-black rounded-0 border-0 text-white">
                 <div class="row">
-                    <img src="./${img}" alt="" class="col-4 img-fluid">
-                    <div class="col-8">
-                        <h2>${name}</h2>
-                        <p>${role}</p>
-                        <a href="#">${email}</a>
+                    <img src="./${img}" alt="" class="col-sm-6 col-md-4 img-fluid">
+                    <div class="col-sm-6 col-md-8 p-md-3 p-4">
+                        <h2 class="h5 fw-bolder">${name}</h2>
+                        <p class="h6 fw-normal">${role}</p>
+                        <a href="#" class="text-decoration-none">${email}</a>
                     </div>
                 </div>
             </div>
