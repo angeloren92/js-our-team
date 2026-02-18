a6b0472db327dadf37c76bbf8e049d27fc99c82c
angeloren92 <angeloren92@gmail.com>
2026-02-18 17:13:24 +0100

    adj bug img

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,7 @@ function getMarkup(arr) {
         <div class="col-md-6 col-xl-4 gy-3">
             <div class="card bg-black rounded-0 border-0 text-white">
                 <div class="row">
-                    <img src="./${img}" alt="" class="col-sm-6 col-md-4 img-fluid">
+                    <img src="${img}" alt="" class="col-sm-6 col-md-4 img-fluid">
                     <div class="col-sm-6 col-md-8 p-md-3 p-4">
                         <h2 class="h5 fw-bolder">${name}</h2>
                         <p class="h6 fw-normal">${role}</p>
